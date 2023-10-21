0cac6d98502984a8ac4f1d7dae9fd5d472d8b492
Naveen Kumar Ongole <naveen.ongole@gmail.com>
2023-10-21 17:10:09 +0530

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Navbar from './components/Navbar/Navbar';
-import Home from './pages/Home';
+import Home from './pages/Home/Home';
+import Cart from './pages/Cart';
+import SignIn from './pages/SignIn/SignIn';
+import SignUp from './pages/SignUp/Signup';
+import Myorders from './pages/Myorders';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Navbar/>,
+      children:[
+        {index:true, element:<Home/>},
+        {path: "/signin", element: <SignIn/>},
+        {path: "/signup", element: <SignUp/>},
+        {path: "/myorders", element: <Myorders/>},
+        {path: "/cart", element: <Cart/>}
+      ]
+    }
+  ])
+
   return (
     <div className="App">
-      <Navbar/>
-      <Home/>
+      <RouterProvider router={router}/>
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,45 +1,57 @@
 import style from "./Navbar.module.css";
+import { Outlet, Link } from "react-router-dom";
 
 const Navbar = ()=> {
-  const isLoggedIn = true;
+  const isLoggedIn = false;
   return (
-    <nav className="navbar navbar-expand-lg bg-light-rgb">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">Busy Buy</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        {isLoggedIn && 
-          <div className="collapse navbar-collapse justify-content-end" id="navbarNav" >
-            <ul className="navbar-nav ">
-              <li className={`nav-item ${style.flex_row}`}>
-                <img src="" alt=""/>
-                <a className="nav-link">Home</a>
-              </li>
-              <li className={`nav-item ${style.flex_row}`}>
-                <img src="" alt=""/>
-                <a className="nav-link">My orders</a>
-              </li>
-              <li className={`nav-item ${style.flex_row}`}>
-                <img src="" alt=""/>
-                <a className="nav-link">Cart</a>
-              </li>
-              <li className={`nav-item ${style.flex_row}`}>
-                <img src="" alt=""/>
-                <a className="nav-link">Logout</a>
-              </li>
-            </ul>
-          </div>
-        }
-        {
-          !isLoggedIn &&
-          <li className={`nav-item ${style.flex_row}`}>
-            <img src="" alt=""/>
-            <a className="nav-link">SignIn</a>
-          </li>
-        }
-      </div>
-    </nav>
+    <>
+      <nav className="navbar navbar-expand-lg bg-light-rgb">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Busy Buy</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          {isLoggedIn && 
+            <div className="collapse navbar-collapse justify-content-end" id="navbarNav" >
+              <ul className="navbar-nav ">
+                <li className={`nav-item ${style.flex_row}`}>
+                  <img src="" alt=""/>
+                  <a className="nav-link">Home</a>
+                </li>
+                <li className={`nav-item ${style.flex_row}`}>
+                  <img src="" alt=""/>
+                  <a className="nav-link">My orders</a>
+                </li>
+                <li className={`nav-item ${style.flex_row}`}>
+                  <img src="" alt=""/>
+                  <a className="nav-link">Cart</a>
+                </li>
+                <li className={`nav-item ${style.flex_row}`}>
+                  <img src="" alt=""/>
+                  <a className="nav-link">Logout</a>
+                </li>
+              </ul>
+            </div>
+          }
+          {
+            !isLoggedIn &&
+            <div className="collapse navbar-collapse justify-content-end" id="navbarNav" >
+              <ul className="navbar-nav ">
+                <li className={`nav-item ${style.flex_row}`}>
+                  <img src="" alt=""/>
+                  <Link className="nav-link" to="/">Home</Link>
+                </li>
+                <li className={`nav-item ${style.flex_row}`}>
+                  <img src="" alt=""/>
+                  <Link className="nav-link" to="/signin">SignIn</Link>
+                </li>
+              </ul>
+            </div>
+          }
+        </div>
+      </nav>
+      <Outlet/>
+    </>
   );
 }
 
