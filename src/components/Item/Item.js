f2cfbf7db8425672aa916c58b8daa053c186d6b3
Naveen Kumar Ongole <naveen.ongole@gmail.com>
2023-10-28 22:42:52 +0530

    fetch data from api, commented hardcard data

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,23 +1,30 @@
 import { useState } from "react";
 import style from "./Item.module.css";
+import { useValue } from "../../itemContext";
+import { useLocation } from "react-router-dom";
 
-const Item = ({imgLink, description, price})=> {
+const Item = ({item})=> {
   const [isClicked, setIsClicked] = useState(false);
+  const value = useValue();
+  const location = useLocation();
+
+  // console.log(location.pathname);
 
   const handleClick = ()=> {
     setIsClicked(true);
     setTimeout(()=> {
       setIsClicked(false);
     }, 2000);
+    value.addItemToCart(item);
   }
 
   return (
     <div className="col">
       <div className={`card ${style.card_self}`}>
-        <img src={imgLink} alt="" className={`card-img-top ${style.img_size}`}/>
+        <img src={item.imgLink} alt="" className={`card-img-top ${style.img_size}`}/>
         <div className="card-body">
-          <span className="d-inline-block text-truncate" style={{maxWidth: 150}}>{description}</span>
-          <p>&#x20B9;{price}</p>  
+          <span className="d-inline-block text-truncate" style={{maxWidth: 150}}>{item.title}</span>
+          <p>&#x20B9;{item.price}</p>  
           <button type="button" className={`btn ${style.btn_color}`}
             onClick={()=> handleClick()}  
           >
--- src/components/ItemContainer/ItemContainer.js
+++ src/components/ItemContainer/ItemContainer.js
@@ -1,40 +1,40 @@
 import Item from "../Item/Item";
 
-const ItemContainer = ()=> {
-  const data = [
-    {
-      link: "https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F9f%2Fdf%2F9fdfcc23665fe9e2b389b686a85cc9c97084d238.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_shirts_longsleeved%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/zoom]&zoom=zoom",
-      description: "M&H Men Black/Natural White Cotton flannel shirt",
-      price: "1400",
-      category: "Clothing",
-      Gender:"Men"
-    },
-    {
-      link: "https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F9f%2Fdf%2F9fdfcc23665fe9e2b389b686a85cc9c97084d238.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_shirts_longsleeved%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/zoom]&zoom=zoom",
-      description: "M&H Men Black/Natural White Cotton flannel shirt",
-      price: "1400",
-      category: "Clothing",
-      Gender:"Men"
-    },
-    {
-      link: "https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F9f%2Fdf%2F9fdfcc23665fe9e2b389b686a85cc9c97084d238.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_shirts_longsleeved%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/zoom]&zoom=zoom",
-      description: "M&H Men Black/Natural White Cotton flannel shirt",
-      price: "1400",
-      category: "Clothing",
-      Gender:"Men"
-    },
-    {
-      link: "https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F9f%2Fdf%2F9fdfcc23665fe9e2b389b686a85cc9c97084d238.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_shirts_longsleeved%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/zoom]&zoom=zoom",
-      description: "M&H Men Black/Natural White Cotton flannel shirt",
-      price: "1400",
-      category: "Clothing",
-      Gender:"Men"
-    }
-  ];
+const ItemContainer = ({data})=> {
+  // const data = [
+  //   {
+  //     link: "https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F9f%2Fdf%2F9fdfcc23665fe9e2b389b686a85cc9c97084d238.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_shirts_longsleeved%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/zoom]&zoom=zoom",
+  //     description: "M&H Men Black/Natural White Cotton flannel shirt",
+  //     price: "1400",
+  //     category: "Clothing",
+  //     Gender:"Men"
+  //   },
+  //   {
+  //     link: "https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F9f%2Fdf%2F9fdfcc23665fe9e2b389b686a85cc9c97084d238.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_shirts_longsleeved%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/zoom]&zoom=zoom",
+  //     description: "M&H Men Black/Natural White Cotton flannel shirt",
+  //     price: "1400",
+  //     category: "Clothing",
+  //     Gender:"Men"
+  //   },
+  //   {
+  //     link: "https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F9f%2Fdf%2F9fdfcc23665fe9e2b389b686a85cc9c97084d238.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_shirts_longsleeved%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/zoom]&zoom=zoom",
+  //     description: "M&H Men Black/Natural White Cotton flannel shirt",
+  //     price: "1400",
+  //     category: "Clothing",
+  //     Gender:"Men"
+  //   },
+  //   {
+  //     link: "https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F9f%2Fdf%2F9fdfcc23665fe9e2b389b686a85cc9c97084d238.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bmen_shirts_longsleeved%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/zoom]&zoom=zoom",
+  //     description: "M&H Men Black/Natural White Cotton flannel shirt",
+  //     price: "1400",
+  //     category: "Clothing",
+  //     Gender:"Men"
+  //   }
+  // ];
   return (
     <div className="col">
       <div className="row row-cols-1 row-cols-md-3 g-4">
-        {data.map((item)=> <Item imgLink={item.link} description={item.description} price={item.price}/>)}
+        {data.map((item)=> <Item key={item.id} item={item}/>)}
       </div>
     </div>
   )
