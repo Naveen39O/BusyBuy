7afa08efca3b638ef9a908c9ceb04379af9bb5fa
Naveen Kumar Ongole <naveen.ongole@gmail.com>
2023-10-28 22:41:09 +0530

    Add event handler for checkbox

--- src/components/CheckBox/CheckBox.js
+++ src/components/CheckBox/CheckBox.js
@@ -1,9 +1,13 @@
+import { useValue } from "../../itemContext";
 
 const CheckBox = ({text, id})=> {
-  
+  const value = useValue();
+
   return (
     <div>
-      <input className="form-check-input" type="checkbox" value="" id={id}/>
+      <input className="form-check-input" type="checkbox" value={id} id={id}
+        onChange = {value.handleCategoryCheck}
+      />
       <label className="form-check-label" for={id}>
         {text}
       </label>
