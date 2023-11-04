9b734e21a465ac78957ffb3afc0fc850003d61d6
Naveen Kumar Ongole <naveen.ongole@gmail.com>
2023-11-04 22:41:53 +0530

    Add buttons for increasing and decreasing items and conditional rendering based on current route

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -2,10 +2,12 @@ import { useState } from "react";
 import style from "./Item.module.css";
 import { useValue } from "../../itemContext";
 import { useLocation } from "react-router-dom";
+import { useAuthValue } from "../../authContext";
 
 const Item = ({item})=> {
   const [isClicked, setIsClicked] = useState(false);
   const value = useValue();
+  const authValue = useAuthValue();
   const location = useLocation();
 
   // console.log(location.pathname);
@@ -15,20 +17,37 @@ const Item = ({item})=> {
     setTimeout(()=> {
       setIsClicked(false);
     }, 2000);
-    value.addItemToCart(item);
+    if(location.pathname == "/cart"){
+      value.removeItemFromCart(item);
+    }else {
+      value.addItemToCart(item);
+    }
+    
   }
 
   return (
     <div className="col">
       <div className={`card ${style.card_self}`}>
-        <img src={item.imgLink} alt="" className={`card-img-top ${style.img_size}`}/>
+        <img src={item.image} alt="" className={`card-img-top ${style.img_size}`}/>
         <div className="card-body">
-          <span className="d-inline-block text-truncate" style={{maxWidth: 150}}>{item.title}</span>
-          <p>&#x20B9;{item.price}</p>  
+          <span className="fs-5 d-inline-block text-truncate" style={{maxWidth: 150}}>{item.title}</span>
+          <div >
+            <span className="fs-5">&#x20B9;{item.price}</span>
+            {
+              location.pathname == "/cart" && 
+              <div className={style.item_count_container}>
+                <i className="fa-solid fa-circle-minus fa-lg"
+                  onClick={()=>value.reduceItemInCart(item)}></i>
+                <span className="fs-5">{item.count}</span>
+                <i className="fa-solid fa-circle-plus fa-lg"
+                  onClick={()=>value.addItemToCart(item)}></i>
+              </div>
+            }
+          </div>  
           <button type="button" className={`btn ${style.btn_color}`}
             onClick={()=> handleClick()}  
           >
-            {isClicked ? "Adding": "Add To Cart"}
+            {(location.pathname == "/cart")? (isClicked ? "Removing": "Remove From Cart"):(isClicked ? "Adding": "Add To Cart")}
           </button>
         </div>
       </div>
