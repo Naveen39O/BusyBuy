8f43eedc669f2a85ef1703b3b6d47308eebdff5b
Naveen Kumar Ongole <naveen.ongole@gmail.com>
2023-11-04 22:36:34 +0530

    pass data to Item container for rendering items

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,14 @@
 import style from "./Home.module.css"
 import Filter from '../../components/Filter/Filter';
 import ItemContainer from '../../components/ItemContainer/ItemContainer';
+import { useValue } from "../../itemContext";
 
 const Home = ()=> {
-
+  const value = useValue();
   return (
     <div className={`${style.home_container}`}>
         <Filter/>
-        <ItemContainer/>
+        <ItemContainer data = {value.filterData}/>
     </div>
   )
 }
