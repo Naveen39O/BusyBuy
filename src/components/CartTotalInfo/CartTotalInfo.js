30335812f3410a79113ed85451c66cf63b29b8ee
Naveen Kumar Ongole <naveen.ongole@gmail.com>
2023-11-04 22:32:42 +0530

    Add Event handler for purchase button

--- src/components/CartTotalInfo/CartTotalInfo.js
+++ src/components/CartTotalInfo/CartTotalInfo.js
@@ -1,7 +1,9 @@
+import { useAuthValue } from "../../authContext";
 import { useValue } from "../../itemContext";
 
 const CartTotalInfo = ()=>{
   const value = useValue();
+  const authValue = useAuthValue();
 
   return(
     <div className="col">
@@ -9,7 +11,11 @@ const CartTotalInfo = ()=>{
         <div className="card-body">
           <span>TotalPrice:-&#x20B9;{value.total}/-</span>
           <div class="d-grid gap-2">
-              <button type="submit" className="btn btn-primary">Purchase</button>
+              <button type="submit" className="btn btn-primary"
+                onClick={()=>value.handlePurchase(authValue.userId, value.cartData, value.total)}
+              >
+                Purchase
+              </button>
           </div>
         </div>
       </div>
